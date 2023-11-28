fecd74df3004393d54747716be02b085111cfab5
Camila Alani <alanicamila124@gmail.com>
2023-11-28 14:28:41 -0300

    agregue una funcion para que cuando el usuario presione iniciar empiec

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 //5- Realizar una web con un cronómetro, que tenga las opciones de iniciar, reset (volver el cronómetro a 0) y pausar.
+
+    let cronometro;
+    let segundos = 0;
+
+        function iniciar() {
+            cronometro = setInterval(function() {
+                segundos++;
+                let horas = Math.floor(segundos / 3600);
+                let minutos = Math.floor((segundos % 3600) / 60);
+                let segundosRestantes = segundos % 60;
+                document.getElementById("cronometro").textContent = 
+                    String(horas).padStart(2, "0") + ":" +
+                    String(minutos).padStart(2, "0") + ":" +
+                    String(segundosRestantes).padStart(2, "0");
+            }, 1000);
+        }
+
+        function pausar() {
+            clearInterval(cronometro);
+        }
+
+        function reset() {
+            clearInterval(cronometro);
+            segundos = 0;
+            document.getElementById("cronometro").textContent = "00:00:00";
+        }
+
